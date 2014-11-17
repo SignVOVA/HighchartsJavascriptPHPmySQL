373da931c82aba4b1c88e84dbbeca873cc646ecc
Volodymyr Maksymchuk <vovamaksymchuk@gmail.com>
2014-11-17 00:53:50 +0000

    index file
    requires config.php with MySQL set-up.
    plus Java script calls

---
+++ index.php
@@ -0,0 +1,24 @@
+  <?php
+  require "config.php";
+  ?>
+
+<html>
+  <head>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.8.2/jquery.min.js"></script>
+  <script src="http://code.highcharts.com/highcharts.js"></script>
+      <h2>PHP Development Test</h2>
+
+    <script src="js/chart2.js"></script>
+    <script src="js/chart3.js"></script>
+    <script src="js/chart1.js"></script>
+
+  </head>
+
+  <body>
+
+  <div id="container2" style="width:100%; height:400px;"></div>
+  <div id="container3" style="width:100%; height:400px;"></div>
+  <div id="container" style="width:100%; height:400px;"></div>
+
+  </body>
+</html>
